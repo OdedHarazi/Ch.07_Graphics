afa04ee7fe0d052196a194104ccb967480401271
haraziode <haraziode@urbandaleschools.com>
2022-11-20 19:05:03 -0600

    NEW RECORD! 13 Lines. Done Parametrically. Update: Fixed a small issue with the stars! Update: 11 Lines of Code! Update: Asks for the size of flag now from the user. Finished my Pycasso - Oded

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -1,11 +1,11 @@
-A=int(input("How big would you like the flag to be? From 1 - 1000:")); D= round(.76*A); B,C,E,F,G,H,K,L,x=(1.9*A),(7/13)*A,(0.054*A),(0.054*A),(0.063*A),(0.063*A),(0.0616*A),round((1/13)*A),D+(A/10)
+A=int(input("How big would you like the flag to be? Enter a good number from 50 - 650:")); D= round(.76*A); B,C,E,F,G,H,K,L,x=(1.9*A),(7/13)*A,(0.054*A),(0.054*A),(0.063*A),(0.063*A),(0.0616*A),round((1/13)*A),D+(A/10)
 import arcade ; arcade.open_window(B,A,"STARS AND STRIPES") ,arcade.set_background_color(arcade.color.WHITE), arcade.start_render()
 for y_offset in range (10,A,L*2): arcade.draw_line(0, y_offset, 2*A, y_offset, (180,10,45), L)
 arcade.draw_rectangle_filled(D/2,A-(C/2),D,C,(0,40,98))
 while x >= D-5*(A/20):
     for z_offset in range(round(H/2),D,round(H*2)):arcade.draw_text("*",z_offset,x,(255,255,255),H*1.5)
     x-=(A/20)
-    for z_offset in range(round(H*3),D,round(H*2)):arcade.draw_text("*",z_offset-(A/10),x,(255,255,255),H*1.5,)
+    for z_offset in range(round(H*3),D,round(H*2),):arcade.draw_text("*",z_offset-(A/10),x,(255,255,255),H*1.5,)
     x-=(A/20)
     for z_offset in range(round(H/2),D,round(H*2)):arcade.draw_text("*",z_offset,x,(255,255,255),H*1.5)
 arcade.finish_render() , arcade.run()
@@ -42,13 +42,13 @@ CHALLENGE: Can you make the entire flag parametrically? This means if I change t
 
 
 
-
-
-
-
-
-
 #
+#
+#
+#
+#
+#
+
 # for z_offset in range(round(H/2),D,round(H*2)):arcade.draw_text("*",z_offset,D+(A/10),(255,255,255),H*1.5)
 # for z_offset in range(round(H/2),D,round(H*2)):arcade.draw_text("*",z_offset,D,(255,255,255),H*1.5)
 # for z_offset in range(round(H/2),D,round(H*2)):arcade.draw_text("*",z_offset,D-(A/10),(255,255,255),H*1.5)
@@ -56,14 +56,15 @@ CHALLENGE: Can you make the entire flag parametrically? This means if I change t
 # for z_offset in range(round(H/2),D,round(H*2)):arcade.draw_text("*",z_offset,D-3*(A/10),(255,255,255),H*1.5)
 #
 #
-# for z_offset in range(round(H),D,round(H*2)):arcade.draw_text("*",z_offset-(A/10),D+(A/20),(255,255,255),H*1.5)
-# for z_offset in range(round(H),D,round(H*2)):arcade.draw_text("*",z_offset-(A/10),D-(A/20),(255,255,255),H*1.5)
-# for z_offset in range(round(H),D,round(H*2)):arcade.draw_text("*",z_offset-(A/10),D-3*(A/20),(255,255,255),H*1.5)
-# for z_offset in range(round(H),D,round(H*2,)):arcade.draw_text("*",z_offset-(A/10),D-5*(A/20),(255,255,255),H*1.5)
-#
+# for z_offset in range(round(H*3),D,round(H*2)):arcade.draw_text("*",z_offset-(A/10),D+(A/20),(255,255,255),H*1.5)
+# for z_offset in range(round(H*3),D,round(H*2)):arcade.draw_text("*",z_offset-(A/10),D-(A/20),(255,255,255),H*1.5)
+# for z_offset in range(round(H*3),D,round(H*2)):arcade.draw_text("*",z_offset-(A/10),D-3*(A/20),(255,255,255),H*1.5)
+# for z_offset in range(round(H*3),D,round(H*2,)):arcade.draw_text("*",z_offset-(A/10),D-5*(A/20),(255,255,255),H*1.5)
 
 
 
+
+arcade.finish_render() , arcade.run()
 
 # while x >= D-5*(A/20):
 #     for z_offset in range(round(H/2),D,round(H*2)):arcade.draw_text("*",z_offset,x,(255,255,255),H*1.5)
--- 7.2_Pycasso.py
+++ 7.2_Pycasso.py
@@ -16,5 +16,47 @@ When you are finished Pull Request your file to your instructor.
 '''
 
 
+import arcade ; arcade.open_window(500,500, "Amongaso") ,arcade.set_background_color(arcade.color.EERIE_BLACK), arcade.start_render()
 
 
+x=-75
+a=255
+b=0
+c=0
+while x<=200:
+    arcade.draw_circle_filled(x+250,250,50,(a,b,c),)
+    arcade.draw_ellipse_filled(x+204,203,15,130,(a,b,c),)
+    arcade.draw_ellipse_filled(x+288,200,20,80,(a,b,c),)
+    arcade.draw_ellipse_filled(x+245,175,25,90,(a,b,c),100,)
+    arcade.draw_ellipse_filled(x+270,250,40,80,arcade.color.COOL_GREY,90,)
+    arcade.draw_ellipse_filled(x+270,255,40,80,arcade.color.COOL_GREY,90,)
+    arcade.draw_ellipse_filled(x+270,260,40,80,arcade.color.COOL_GREY,90,)
+    arcade.draw_rectangle_filled(x+245,200,95,60,(a,b,c),)
+    arcade.draw_ellipse_outline(x+270,255,53,83,arcade.color.BLACK,3,90,)
+    arcade.draw_rectangle_filled(x+195,212,15,54,(a,b,c),)
+    arcade.draw_circle_filled(x+203,242,15,(a,b,c),)
+    arcade.draw_circle_filled(x+202,192,15,(a,b,c),)
+
+    arcade.draw_rectangle_filled(x+225,160,32.5,40,(a,b,c),)
+    arcade.draw_ellipse_filled(x+221.5,145,30,37.5,(a,b,c),90,)
+
+    arcade.draw_rectangle_filled(x+275,160,37.5,40,(a,b,c),355)
+    arcade.draw_ellipse_filled(x+276.5,145,30,37.5,(a,b,c),85,)
+    arcade.draw_text("AMONGASO",100,375,arcade.color.GREEN,30)
+    arcade.draw_text("Oded's",100,425,arcade.color.PINK_SHERBET,15)
+    arcade.draw_text("WHO IS THE IMPOSTER???",120,65,arcade.color.YELLOW,15)
+
+    x+=200
+    a=0
+    c=255
+
+arcade.finish_render(),arcade.run()
+
+
+
+# arcade.draw_parabola_outline(227,225,263,30,arcade.color.BLACK,10,90,)
+# arcade.draw_parabola_outline(277,225,313,30,arcade.color.BLACK,10,270,)
+# arcade.draw_parabola_outline(232,230,310,32.5,arcade.color.BLACK,10,)
+# arcade.draw_parabola_outline(232,217,310,30,arcade.color.BLACK,10,180)
+
+# arcade.draw_line(200,250,200,125,arcade.color.RED,3)
